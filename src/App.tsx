f7ff7d563f74c59d2c01e85a7b187c38f4446818
ni-azhukov <alexander.zhukov@native-instruments.de>
2022-01-27 13:24:26 +0100

    add visual editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import React from "react";
+import Editor from "./Editor";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>[in] --- [ web noise] --- [out]</p>
-        <a
-          className="App-link"
-          href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_APIhttps://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Web Audio Api
-        </a>
-      </header>
+      <main className="Editor">
+        <Editor />
+      </main>
     </div>
   );
 }
