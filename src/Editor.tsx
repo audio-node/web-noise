1d63b76cc74d254ad7e44378f950fd84377c9c5c
ni-azhukov <alexander.zhukov@native-instruments.de>
2022-01-27 14:55:26 +0100

    add hooks and handlers

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,8 +1,12 @@
+import React, { useState, useEffect, useCallback } from "react";
 import ReactFlow, {
   Background,
   BackgroundVariant,
   MiniMap,
   Controls,
+  isEdge,
+  removeElements,
+  addEdge,
 } from "react-flow-renderer";
 import MultiHandlesNode from "./MultiHandlesNode";
 
@@ -10,7 +14,7 @@ const nodeTypes = {
   multiHandlesNode: MultiHandlesNode,
 };
 
-const elements = [
+const initialElements = [
   {
     id: "1",
     type: "input", // input node
@@ -42,12 +46,91 @@ const elements = [
   { id: "e2-3", source: "2", target: "3" },
 ];
 
-export const Editor = () => (
-  <ReactFlow elements={elements} nodeTypes={nodeTypes}>
-    <Background variant={BackgroundVariant.Dots} gap={12} />
-    <MiniMap />
-    <Controls />
-  </ReactFlow>
-);
+const onNodeDragStop = (_event: any, node: any) =>
+  console.log("drag stop", node);
+const onElementClick = (_event: any, element: any) =>
+  console.log("click", element);
+
+const snapGrid: [number, number] = [20, 20];
+
+export const Editor = () => {
+  const [reactflowInstance, setReactflowInstance] = useState(null);
+  const [elements, setElements] = useState(initialElements);
+
+  useEffect(() => {
+    const onChange = (event: any) => {
+      console.log(234234);
+      setElements((els: any) =>
+        els.map((e: any) => {
+          if (isEdge(e) || e.id !== "2") {
+            return e;
+          }
+
+          const color = event.target.value;
+
+          return {
+            ...e,
+            data: {
+              ...e.data,
+              color,
+            },
+          };
+        })
+      );
+    };
+
+    setElements(elements);
+  }, []);
+
+  useEffect(() => {
+    if (reactflowInstance && elements.length > 0) {
+      // @ts-ignore
+      reactflowInstance.fitView();
+    }
+  }, [reactflowInstance, elements.length]);
+
+  const onElementsRemove = useCallback(
+    (elementsToRemove) =>
+      // @ts-ignore
+      setElements((els) => removeElements(elementsToRemove, els)),
+    []
+  );
+  const onConnect = useCallback(
+    (params) =>
+      // @ts-ignore
+      setElements((els) =>
+        addEdge({ ...params, animated: true, style: { stroke: "#fff" } }, els)
+      ),
+    []
+  );
+
+  const onLoad = useCallback(
+    (rfi) => {
+      if (!reactflowInstance) {
+        setReactflowInstance(rfi);
+        console.log("flow loaded:", rfi);
+      }
+    },
+    [reactflowInstance]
+  );
+  return (
+    <ReactFlow
+      elements={elements}
+      onElementClick={onElementClick}
+      onElementsRemove={onElementsRemove}
+      onConnect={onConnect}
+      onNodeDragStop={onNodeDragStop}
+      onLoad={onLoad}
+      nodeTypes={nodeTypes}
+      snapToGrid={true}
+      snapGrid={snapGrid}
+      defaultZoom={1.5}
+    >
+      <Background variant={BackgroundVariant.Dots} gap={12} />
+      <MiniMap />
+      <Controls />
+    </ReactFlow>
+  );
+};
 
 export default Editor;
