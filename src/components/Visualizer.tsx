a585ecc4432c5fe8152d9c71176991baddf06acf
ni-azhukov <alexander.zhukov@native-instruments.de>
2022-02-06 17:07:15 +0100

    visualise white noise

--- src/components/Visualizer.tsx
+++ src/components/Visualizer.tsx
@@ -86,7 +86,7 @@ const Visualizer = ({
   useAnimationFrame(tick);
   return (
     <>
-      <div>visualiser</div>
+      <div>{data.label || "visualiser"}</div>
       <div>
         <Handle
           id="in"
