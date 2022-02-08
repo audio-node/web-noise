5f1c9ed48f3ac47188db3aa40a5e270ca7f364aa
renoiser <renato.longobardi@gmail.com>
2022-02-08 22:27:02 +0100

    update editor

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -15,8 +15,13 @@ import Gain from "./components/Gain";
 import Wire from "./components/Wire";
 import Visualizer from "./components/Visualizer";
 import WhiteNoise from "./components/WhiteNoise";
+import Filter from "./components/Filter";
 import { EditorContext, contextValue } from "./components/EditorContext";
 import ResumeContext from "./components/ResumeContext";
+
+import { filterExample } from "./editorExamples";
+
+const initialElements: Elements = filterExample;
 
 const nodeTypes = {
   multiHandlesNode: MultiHandlesNode,
@@ -25,106 +30,12 @@ const nodeTypes = {
   visualiser: Visualizer,
   destination: Destination,
   whiteNoise: WhiteNoise,
+  filter: Filter,
 };
 
 const edgeTypes = {
   wire: Wire,
 };
-
-const spaceWidth = 230;
-
-const initialElements: Elements = [
-  {
-    id: "oscillator",
-    type: "oscillator",
-    data: { label: "Oscillator" },
-    position: { x: 0, y: -50 },
-    className: "react-flow__node-default",
-  },
-  {
-    id: "lfo",
-    type: "oscillator",
-    data: { label: "LFO" },
-    position: { x: 0, y: 50 },
-    className: "react-flow__node-default",
-  },
-  {
-    id: "gain",
-    type: "gain",
-    dragHandle: ".dragHandle",
-    data: { label: "Gain" },
-    position: { x: spaceWidth, y: 25 },
-    className: "react-flow__node-default",
-  },
-  {
-    id: "visualiser",
-    type: "visualiser",
-    data: { label: "Visualiser" },
-    position: { x: spaceWidth * 2, y: 100 },
-    className: "react-flow__node-default",
-  },
-  {
-    id: "visualiser2",
-    type: "visualiser",
-    data: { label: "Visualiser 2" },
-    position: { x: spaceWidth * 2, y: -30 },
-    className: "react-flow__node-default",
-  },
-  {
-    id: "destination",
-    type: "destination",
-    data: { label: "Oscillator" },
-    position: { x: spaceWidth * 3, y: 25 },
-    className: "react-flow__node-default",
-  },
-  {
-    id: "whiteNoise",
-    type: "whiteNoise",
-    data: { label: "White Noise" },
-    position: { x: spaceWidth, y: -30 },
-    className: "react-flow__node-default",
-  },
-  {
-    id: "osc-gain",
-    source: "oscillator",
-    target: "gain",
-    type: "wire",
-    targetHandle: "in",
-    sourceHandle: "out",
-  },
-  {
-    id: "lfo-gain",
-    source: "lfo",
-    target: "gain",
-    type: "wire",
-    targetHandle: "gain",
-    sourceHandle: "out",
-  },
-  {
-    id: "gain-to-vis",
-    source: "gain",
-    target: "visualiser",
-    type: "wire",
-    targetHandle: "in",
-    sourceHandle: "out",
-  },
-  {
-    id: "wn-to-vis",
-    source: "whiteNoise",
-    target: "visualiser2",
-    type: "wire",
-    targetHandle: "in",
-    sourceHandle: "out",
-  },
-  {
-    id: "vis-to-dest",
-    source: "visualiser",
-    target: "destination",
-    type: "wire",
-    targetHandle: "in",
-    sourceHandle: "out",
-  },
-];
 
 const onNodeDragStop = (_event: any, node: any) =>
   console.log("drag stop", node);
