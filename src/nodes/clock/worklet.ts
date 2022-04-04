63873848b5ac76ff070c5649e4e9317d55768af3
ni-azhukov <alexander.zhukov@native-instruments.de>
2022-04-04 12:47:00 +0200

    handle initial future tick time

--- src/nodes/clock/worklet.ts
+++ src/nodes/clock/worklet.ts
@@ -1,10 +1,10 @@
 export class ClockProcessor extends AudioWorkletProcessor {
   counter: number;
-  futureTickTime: number;
+  futureTickTime: number | null;
   constructor() {
     super();
     this.counter = 1;
-    this.futureTickTime = 1;
+    this.futureTickTime = null;
   }
 
   static get parameterDescriptors() {
@@ -22,6 +22,10 @@ export class ClockProcessor extends AudioWorkletProcessor {
   process(_inputs: any, _outputs: any, parameters: any) {
     const secondsPerBeat = 60 / parameters["tempo"][0];
     const counterTimeValue = secondsPerBeat / 4;
+
+    if (this.futureTickTime === null) {
+      this.futureTickTime = counterTimeValue;
+    }
 
     if (this.futureTickTime < currentTime + 0.1) {
       console.log("This is 16th note: " + this.counter);
