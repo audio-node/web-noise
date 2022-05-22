11bec3b8facf421af212f67f8b8f7ddaf5bf48d5
renoiser <renato.longobardi@gmail.com>
2022-05-22 14:02:15 +0200

    update interaction logic

--- src/components/StepSequencer/index.tsx
+++ src/components/StepSequencer/index.tsx
@@ -19,33 +19,45 @@ interface StepData {
 
 const StepSequencer = ({ id, data }: NodeProps) => {
   const levaStore = useCreateStore();
-  const [gridData, setgridData] = useState<any[]>(
-    new Array(GRID_NUMBER).fill({ value: null, active: false, selected: false })
+  const [gridData, setgridData] = useState<StepData[]>(
+    new Array(GRID_NUMBER).fill({ value: -1, active: false, selected: false })
   );
   const [isMousePressed, setIsMousePressed] = useState(false);
   const [mouseDownXY, setMouseDownXY] = useState({ x: 0, y: 0 });
-  const [deltaXY, setDeltaXY] = useState({ x: 0, y: 0 });
+  const [delta, setDelta] = useState(0);
+  const [selectedStep, setSelectedStep] = useState<number | null>(null);
+  const [selectedStepValue, setSelectedStepValue] =
+    useState<number | null>(null);
 
-  const onMouseDown = (e: MouseEvent) => {
+  const onMouseDown = (e: MouseEvent): void => {
     setIsMousePressed(true);
     setMouseDownXY({ x: e.clientX, y: e.clientY });
   };
 
-  const onMouseUp = (e: MouseEvent) => {
+  const onMouseUp = (e: MouseEvent): void => {
     setIsMousePressed(false);
+    setSelectedStep(null);
+    setDelta(0);
   };
 
-  const onMouseMove = (e: MouseEvent) => {
+  const onMouseMove = function (e: MouseEvent): void {
     const delta = mouseDownXY.y - e.clientY;
-    const target = e.target as HTMLElement;
+    setDelta(delta);
+  };
 
-    if (delta >= 0 && delta <= 127) {
-      console.log(delta, Midi.midiToNoteName(delta));
-    }
+  const updateStep = (index: number, value: number): void => {
+    const newGrid = gridData.map((step, stepIdx) => {
+      if (stepIdx === index) {
+        const updatedStep = {
+          ...step,
+          value,
+        };
+        return updatedStep;
+      }
+      return step;
+    });
 
-    if (delta < 0) {
-      console.log("X");
-    }
+    setgridData(newGrid);
   };
 
   useEffect(() => {
@@ -66,34 +78,45 @@ const StepSequencer = ({ id, data }: NodeProps) => {
     };
   }, [mouseDownXY, isMousePressed]);
 
-  // const onStepClick = (index: number) => {
-  //   console.log("sdfds");
+  useEffect(() => {
+    if (selectedStep && selectedStepValue) {
+      let newValue = selectedStepValue + delta;
 
-  //   // const newGrid = gridData.map((step, stepIdx) => {
-  //   //   if (stepIdx === index) {
-  //   //     const updatedStep = {
-  //   //       ...step,
-  //   //       value: "A1",
-  //   //     };
-  //   //     return updatedStep;
-  //   //   }
-  //   //   return step;
-  //   // });
-  //   // setgridData(newGrid);
-  // };
+      if (newValue >= -1 && newValue <= 127) {
+        updateStep(selectedStep, newValue);
+      }
+    }
+  }, [delta, selectedStep, selectedStepValue]);
+
+  // TODO: convert to component
+  const formatStepValue = (value: number) => {
+    if (value < 0) {
+      return "-";
+    }
+
+    if (value) {
+      return Midi.midiToNoteName(value);
+    }
+  };
 
   return (
     <Node title={data.label}>
       <LevaPanel store={levaStore} fill flat hideCopyButton titleBar={false} />
       {isMousePressed ? "pressed" : "not-pressed"}
+      <p>step: {selectedStep}</p>
+      <p>delta: {delta}</p>
       <Grid>
         {gridData.map((el, idx) => {
           return (
-            <StepBlock key={`step-${idx}`} id={`step-${idx}`}>
-              <>
-                {el.value !== null ? el.value : "-"}
-                {/* <Step></Step> */}
-              </>
+            <StepBlock
+              onMouseDown={() => {
+                setSelectedStep(idx);
+                setSelectedStepValue(gridData[idx].value);
+              }}
+              key={`step-${idx}`}
+              id={`step-${idx}`}
+            >
+              {formatStepValue(el.value)}
               {/* <input
                 type="range"
                 id="volume"
@@ -117,11 +140,14 @@ const Grid = styled.div`
 const StepBlock = styled.div`
   display: flex;
   justify-content: center;
-  padding: 0.5em;
+  /* padding: 0.5em; */
   font-size: 0.8em;
   box-sizing: border-box;
   font-size: 8px;
   border: 1px solid transparent;
+  min-width: 25px;
+  min-height: 25px;
+  align-items: center;
   &:hover {
     border-color: gray;
     cursor: pointer;
