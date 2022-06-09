e658874106a4cbad85bf470bc133e21932d0770d
ni-azhukov <alexander.zhukov@native-instruments.de>
2022-06-09 21:19:59 +0200

    fix

--- src/components/Visualizer/renderer.worker.js
+++ src/components/Visualizer/renderer.worker.js
@@ -10,30 +10,25 @@ const render = () => {
   }
 
   const canvas = canvasContext.canvas;
+  const { width, height } = canvas;
 
   const bufferLength = audioData.length;
 
-  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+  canvasContext.clearRect(0, height / 2, width, -height);
 
   canvasContext.lineWidth = 1;
   canvasContext.strokeStyle = color;
 
   canvasContext.beginPath();
 
-  const sliceWidth = (canvas.width * 1.0) / bufferLength;
-  let x = 0;
+  const sliceWidth = (width * 1.0) / bufferLength;
 
-  for (let i = 0; i < bufferLength; i++) {
-    const v = audioData[i] + 1;
-    const y = (v * canvas.height) / 2;
+  canvasContext.moveTo(0, (audioData[0] * height) / 2);
+  for (let i = 1; i < bufferLength; i++) {
+    const x = i * sliceWidth;
+    const y = (audioData[i] * height) / 2;
 
-    if (i === 0) {
-      canvasContext.moveTo(x, y);
-    } else {
-      canvasContext.lineTo(x, y);
-    }
-
-    x += sliceWidth;
+    canvasContext.lineTo(x, y);
   }
 
   canvasContext.stroke();
@@ -44,7 +39,7 @@ onmessage = function (evt) {
   if (evt.data.name === "INIT") {
     canvas = evt.data.canvas;
     canvasContext = canvas.getContext("2d");
-    canvasContext.transform(1, 0, 0, -1, 0, canvas.height);
+    canvasContext.transform(1, 0, 0, -1, 0, canvas.height / 2);
     evt.data.port.onmessage = ({ data }) => {
       audioData = new Float32Array(data.input);
     };
