d275257c0a5a241d32c0afcdd3143e43fd98c426
Alexander Zhukov <al.zhukoff@gmail.com>
2022-07-06 20:41:09 +0200

    move reverb logic into audio node (#37)
    Co-authored-by: ni-azhukov <alexander.zhukov@native-instruments.de>

--- src/components/Reverb/index.tsx
+++ src/components/Reverb/index.tsx
@@ -1,34 +1,44 @@
-/* A convolution reverb */
-import { useEffect, useState, VoidFunctionComponent } from "react";
-import { Handle, NodeProps, Position } from "react-flow-renderer";
 import { LevaPanel, useControls, useCreateStore } from "leva";
+import { FC, useEffect, useState } from "react";
+import { NodeProps } from "react-flow-renderer";
+import useFlowNode from "../../hooks/useFlowNode";
+import { useNode } from "../../ModuleContext";
+import { Reverb as TReverb, ReverbValues } from "../../nodes/reverb";
+import { Node } from "../Node";
 
-import { useModule, useNode } from "../../ModuleContext";
-import { Reverb as TReverb } from "../../nodes";
-// @ts-expect-error
-import reverbImpulse from "./impulse.wav";
 
-const Reverb: VoidFunctionComponent<NodeProps> = ({
+interface ReverbData {
+  label: string;
+  values?: ReverbValues;
+}
+
+const Reverb: FC<NodeProps<ReverbData>> = ({
   id,
   data,
-  sourcePosition,
-  targetPosition,
 }) => {
-  const { audioContext } = useModule();
-  const [impulseBuffer, setImpulseBuffer] = useState<AudioBuffer>();
-  // const reverb = useReverb(audioContext, impulseBuffer);
-  const { node: reverb } = useNode<TReverb>(id);
+  const { node } = useNode<Promise<TReverb>>(id);
+
+  const [reverb, setReverb] = useState<TReverb | null>(null);
+  const [ready, setReady] = useState<boolean>(false);
+
+  const { updateNodeValues } = useFlowNode(id);
+
   const store = useCreateStore();
 
   useEffect(() => {
-    //TODO: maybe makes sense to move to audio node?
-    loadImpulse(audioContext).then(setImpulseBuffer);
-  }, [audioContext]);
+    node?.then((result) => {
+      setReverb(result);
+      setReady(true);
+    });
+  }, [node, setReady]);
 
-  const controls = useControls(
+  const { wetDry = 0.5 } =
+    data.values || {};
+
+  const values = useControls(
     {
       wetDry: {
-        value: 0.5,
+        value: wetDry,
         max: 1,
         min: 0,
         label: "wet/dry",
@@ -37,61 +47,23 @@ const Reverb: VoidFunctionComponent<NodeProps> = ({
     { store }
   );
 
-  useEffect(() => {
-    if (!reverb) return;
-
-    reverb.dryGain.gain.setValueAtTime(0.5, audioContext.currentTime);
-    reverb.wetGain.gain.setValueAtTime(0.5, audioContext.currentTime);
-  }, [reverb, id, audioContext]);
-
-  useEffect(() => {
-    if (!reverb) return;
-
-    reverb.wetGain.gain.setValueAtTime(
-      controls.wetDry,
-      audioContext.currentTime
-    );
-    reverb.dryGain.gain.setValueAtTime(
-      1 - controls.wetDry,
-      audioContext.currentTime
-    );
-  }, [controls.wetDry, reverb, audioContext]);
-
-  useEffect(() => {
-    if (!reverb) return;
-
-    reverb.convolver.buffer = impulseBuffer ?? null;
-  }, [reverb, impulseBuffer]);
+  useEffect(() => reverb?.setValues(data.values), [reverb, data]);
+  useEffect(() => updateNodeValues(values), [values]);
 
   return (
-    <>
-      <LevaPanel
-        store={store}
-        fill
-        flat
-        titleBar={{ drag: false, title: data.label }}
-      />
-      <Handle
-        type="target"
-        position={targetPosition || Position.Left}
-        id="in"
-        onConnect={(params) => console.log("handle onConnect", params)}
-      />
-      <Handle
-        type="source"
-        position={sourcePosition || Position.Right}
-        id="out"
-        onConnect={(params) => console.log("handle onConnect", params)}
-      />
-    </>
+    <Node
+      id={id}
+      title={data.label}
+      inputs={reverb?.inputs}
+      outputs={reverb?.outputs}
+    >
+      {ready ? (
+        <LevaPanel store={store} fill flat hideCopyButton titleBar={false} />
+      ) : (
+        <div>loading</div>
+      )}
+    </Node>
   );
 };
 
 export default Reverb;
-
-const loadImpulse = async (audioContext: AudioContext) => {
-  const response = await fetch(reverbImpulse);
-  const buffer = await response.arrayBuffer();
-
-  return audioContext.decodeAudioData(buffer);
-};
