8e27669017fed277dbb3743aeed735f9ede368c0
ni-azhukov <alexander.zhukov@native-instruments.de>
2022-09-05 23:58:05 +0200

    extract base node packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { RecoilRoot } from "recoil";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
@@ -7,9 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <RecoilRoot>
       <App />
-    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
 );
