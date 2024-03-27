3d87d366cee5031b5a0e8bbf183e9b4f595a9e04
Aleksandr Zhukov <aleksandrzhukov@00008809s-MBP.lan>
2024-03-27 17:31:48 +0100

    feat(Base Nodes, Distortion): add Fuzz distortion curve

--- packages/base-nodes/src/Distortion/waveGenerators.ts
+++ packages/base-nodes/src/Distortion/waveGenerators.ts
@@ -49,4 +49,33 @@ export const generateAnalogWarmWave = (
   return generateWave(points, "monotonic", bufferSize);
 };
 
-export const generators = [generateClipperWave, generateAnalogWarmWave];
+export const generateFuzzWave = (
+  drive: number = 0,
+  bufferSize: number = 1024,
+) => {
+  const x1 = scale(drive, 0, 1, 0, 0.25);
+  const x2 = scale(drive, 0, 1, 0.25, 0.32);
+  const x3 = scale(drive, 0, 1, 1, 0.6);
+  const points = [
+    {
+      x: x1,
+      y: 0,
+    },
+    {
+      x: x2,
+      y: 0.25,
+    },
+    {
+      x: 0.5,
+      y: 0.5,
+    },
+    {
+      x: x3,
+      y: 1,
+    },
+  ];
+
+  return generateWave(points, "monotonic", bufferSize);
+};
+
+export const generators = [generateClipperWave, generateAnalogWarmWave, generateFuzzWave];
