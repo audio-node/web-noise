21089090225b58b9c05385e28feecb7074e6520a
Aleksandr Zhukov <aleksandrzhukov@00008811s-Air.lan>
2024-07-01 00:57:04 +0200

    fix(PassThrough): use native gain node in passthrough

--- packages/base-nodes/src/PassThrough/audioNode.ts
+++ packages/base-nodes/src/PassThrough/audioNode.ts
@@ -1,20 +1,12 @@
 import type { WNAudioNode } from "@web-noise/core";
 
-//@ts-ignore
-import passThroughWorkerUrl from "worklet:./worklet.ts";
-
-const passThroughWorker = new URL(passThroughWorkerUrl, import.meta.url);
-
 export interface PassThrough extends WNAudioNode {}
 
 export const passThrough = async (
   audioContext: AudioContext,
 ): Promise<PassThrough> => {
-  await audioContext.audioWorklet.addModule(passThroughWorker);
-  const passThroughNode = new AudioWorkletNode(
-    audioContext,
-    "pass-through-processor",
-  );
+  const passThroughNode = audioContext.createGain();
+  passThroughNode.gain.value = 1;
 
   return {
     inputs: {
