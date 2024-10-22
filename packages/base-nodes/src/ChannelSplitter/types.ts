8ad6177e5bc7c0436642cbadd67d9f66b1c3ff46
Aleksandr Zhukov <aleksandrzhukov@00008809s-MBP.lan>
2024-10-23 01:50:03 +0200

    fix types

--- packages/base-nodes/src/ChannelSplitter/types.ts
+++ packages/base-nodes/src/ChannelSplitter/types.ts
@@ -1,13 +1,13 @@
 import { WNNodeProps, WNNodeData } from "@web-noise/core";
 
-export interface ChannelSplitterValues {
-  value?: number;
-}
+export interface ChannelSplitterValues {}
 
-export interface ChannelSplitterConfig {
-  //your config here
-}
+export interface ChannelSplitterConfig {}
 
-export type ChannelSplitterData = WNNodeData<ChannelSplitterValues, ChannelSplitterConfig>;
+export type ChannelSplitterData = WNNodeData<
+  ChannelSplitterValues,
+  ChannelSplitterConfig
+>;
 
-export interface ChannelSplitterProps extends WNNodeProps<ChannelSplitterData> {};
+export interface ChannelSplitterProps
+  extends WNNodeProps<ChannelSplitterData> {}
